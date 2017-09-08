d7ab7570d773ea969afb9438fd09e2052f71d557
dogusumit <31733984+dogusumit@users.noreply.github.com>
2017-09-08 12:37:07 +0300

    Add files via upload

---
+++ MainForm.Designer.cs
@@ -0,0 +1,221 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: acer
+ * Date: 22.06.2017
+ * Time: 09:54
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace Video_Döndürme_Çevirme
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.LinkLabel linkLabel1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.ComboBox comboBox2;
+		private System.Windows.Forms.ComboBox comboBox3;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.Black;
+            this.linkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.DisabledLinkColor = System.Drawing.Color.Black;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.linkLabel1.LinkColor = System.Drawing.Color.Black;
+            this.linkLabel1.Location = new System.Drawing.Point(534, 414);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(78, 13);
+            this.linkLabel1.TabIndex = 0;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "dogusumit.com";
+            this.linkLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.linkLabel1.VisitedLinkColor = System.Drawing.Color.Black;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkLabel1LinkClicked);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Video Seç..";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(537, 377);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Değiştir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(12, 377);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(516, 23);
+            this.textBox1.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(599, 329);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(12, 406);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Başla";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(156, 414);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Problem yaşarsanız codec değiştirin";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Enabled = false;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "90° Çevir",
+            "180° Çevir",
+            "270° Çevir",
+            "Dikey Aynala",
+            "Yatay Aynala"});
+            this.comboBox1.Location = new System.Drawing.Point(112, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 8;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Default",
+            "libx264",
+            "mpeg4",
+            "libx265",
+            "flv",
+            "libwebp",
+            "wmv3"});
+            this.comboBox2.Location = new System.Drawing.Point(336, 411);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(93, 21);
+            this.comboBox2.TabIndex = 9;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.ComboBox2SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Default",
+            "aac",
+            "aac_fixed",
+            "ac3",
+            "ac3_fixed",
+            "libmp3lame",
+            "wavpack",
+            "libwavpack"});
+            this.comboBox3.Location = new System.Drawing.Point(435, 411);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(93, 21);
+            this.comboBox3.TabIndex = 10;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.ComboBox3SelectedIndexChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.linkLabel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Video Döndürme - Çevirme -  Aynalama";
+            this.Load += new System.EventHandler(this.MainFormLoad);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+		}
+	}
+}
---
+++ MainForm.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Drawing;
+
+namespace Video_Döndürme_Çevirme
+{
+	public partial class MainForm : Form
+	{
+		string girdiYolu,ciktiKlasor,ciktiDosya;
+		string konum,param;
+		string vcodec,acodec;
+		string thumbnailPath;
+		
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			try {
+				
+				OpenFileDialog file = new OpenFileDialog();  
+			    //file.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+			    file.RestoreDirectory = true;   
+				file.CheckFileExists = false;		    
+				file.Title = "Video Seç.."; 
+				file.Multiselect = false;
+		        if (file.ShowDialog() == DialogResult.OK)  
+			    {  
+			        girdiYolu = file.FileName;  
+			        thumbnailPath = Application.StartupPath + "\\1.jpg";
+		            ProcessStartInfo startInfo = new ProcessStartInfo();
+			        startInfo.CreateNoWindow = false;
+			        startInfo.UseShellExecute = false;
+			        startInfo.FileName = Application.StartupPath + "\\ffmpeg.exe";
+			        startInfo.WindowStyle = ProcessWindowStyle.Hidden;
+			        startInfo.Arguments = "-y -i " + "\"" + girdiYolu+ "\"" + " -vframes 1 " + "\"" + thumbnailPath+ "\""; 
+			        
+			            using (Process exeProcess = Process.Start(startInfo))
+			            {
+			                exeProcess.WaitForExit();
+			            }
+			       
+					pictureBox1.ImageLocation = thumbnailPath;
+					pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+					
+					comboBox1.Enabled = true;
+					comboBox1.SelectedIndex = -1;
+					
+					button2.Enabled = false;
+					button3.Enabled = false;
+			    }
+         	} catch (Exception ex){
+	        	MessageBox.Show(ex.Message.ToString());
+	        }	
+		}
+
+		void Button2Click(object sender, EventArgs e)
+		{
+			try {
+				var Klasor = new FolderBrowserDialog();  
+			    Klasor.RootFolder = Environment.SpecialFolder.Desktop;  
+		    	DialogResult result=Klasor.ShowDialog();
+				if (result==DialogResult.OK) {
+			  		ciktiKlasor = Klasor.SelectedPath;
+			  		textBox1.Text = ciktiKlasor + "\\" + ciktiDosya;
+				}
+			} catch (Exception ex) {
+				MessageBox.Show(ex.Message.ToString());
+			}
+		}
+		
+		void Button3Click(object sender, EventArgs e)
+		{
+			try
+	        {
+		        ProcessStartInfo startInfo = new ProcessStartInfo();
+		        startInfo.CreateNoWindow = false;
+		        startInfo.UseShellExecute = false;
+		        startInfo.FileName = Application.StartupPath + "\\ffmpeg.exe";
+		        startInfo.WindowStyle = ProcessWindowStyle.Normal;
+		        startInfo.Arguments = "-y -i "+girdiYolu+vcodec+" "+acodec+"-vf \""+param+"\" "+ciktiKlasor+"\\"+ciktiDosya;
+		        
+	            using (Process exeProcess = Process.Start(startInfo)) {
+	                exeProcess.WaitForExit();
+	            }
+	        } catch (Exception ex) {
+	        	MessageBox.Show(ex.Message.ToString());
+	        }
+		}
+
+		
+		
+		
+		void LinkLabel1LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+		{
+			try {
+				System.Diagnostics.Process.Start("http://www.dogusumit.com/");
+			} catch (Exception ex) {
+				MessageBox.Show(ex.Message.ToString());
+			}
+		}
+		void ComboBox1SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if(comboBox1.SelectedIndex > -1)
+			{
+				var bitmap1 = (Bitmap)Bitmap.FromFile(thumbnailPath);
+				
+							switch (comboBox1.SelectedIndex) {
+				case 0:
+					param = "transpose=1";
+					bitmap1.RotateFlip(RotateFlipType.Rotate90FlipNone);
+					konum = "90";
+					break;
+				case 1:
+					param = "transpose=2,transpose=2";
+					bitmap1.RotateFlip(RotateFlipType.Rotate180FlipNone);
+					konum = "180";
+					break;
+                case 2:
+					param = "transpose=2";
+					bitmap1.RotateFlip(RotateFlipType.Rotate270FlipNone);
+					konum = "270";
+					break;
+				case 3:
+					param = "vflip";
+					bitmap1.RotateFlip(RotateFlipType.RotateNoneFlipY); 
+					konum = "dikey";
+					break;
+				case 4:
+					param = "hflip";
+					bitmap1.RotateFlip(RotateFlipType.RotateNoneFlipX); 
+					konum = "yatay";
+					break;
+			}
+				
+				ciktiDosya =  Path.GetFileNameWithoutExtension(girdiYolu) + "_" + konum + Path.GetExtension(girdiYolu); 
+				textBox1.Text = "Şuraya kaydedilecek : Masaüstü\\" + ciktiDosya ;
+				
+				ciktiKlasor = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+				
+				pictureBox1.Image = bitmap1;
+				button2.Enabled = true;
+				button3.Enabled = true;
+			}
+			
+
+	
+		}
+		void ComboBox2SelectedIndexChanged(object sender, EventArgs e)
+		{
+			if(comboBox2.SelectedIndex > -1)
+			{
+				if (comboBox2.SelectedIndex == 0) 
+					vcodec = "";
+				else
+					vcodec = " -c:v " + comboBox2.SelectedItem.ToString() + " ";
+			}
+		}
+		void ComboBox3SelectedIndexChanged(object sender, EventArgs e)
+		{
+				if(comboBox3.SelectedIndex > -1)
+			{
+				if (comboBox3.SelectedIndex == 0) 
+					acodec = "";
+				else
+					acodec = " -c:a " + comboBox3.SelectedItem.ToString() + " ";
+			}
+		}
+	}
+}
